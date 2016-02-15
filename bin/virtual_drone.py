6a1344632fcfaa4c73ffe5d4f6c9948f114b07e3
Arthur <arthur@IT024165.(none)>
2016-02-15 17:52:23 +0000

    Arthur fiddled with panel and drone model a bit.

--- bin/virtual_drone.py
+++ bin/virtual_drone.py
@@ -9,6 +9,7 @@ from geometry_msgs.msg import Transform
 from geometry_msgs.msg import TransformStamped
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from math import cos, sin
+from sensor_msgs.msg import JointState
 
 # parent frame name, in which the virtual drone moves
 global_frame = "world"
@@ -17,6 +18,13 @@ drone_frame = "virtual_drone"
 # velocity scaling
 ref_vel_scale = 2.0
 ref_yaw_scale = 2.0
+
+# joint state message
+js_msg = JointState()
+js_msg.name=["flightx","flighty","flightz","yaw"]
+js_msg.position = [0.0, 0.0, 0.0, 0.0]
+js_msg.velocity = [0.0, 0.0, 0.0, 0.0]
+js_msg.effort = [0.0, 0.0, 0.0, 0.0]
 
 def saturate(inp,limit):
   # limit quantity to [-limit,limit]
@@ -30,6 +38,7 @@ def saturate(inp,limit):
 def refUpdate():
   # need to explicitly ref global as I'm writing to it
   global ref_transform
+  global js_msg
   # extract Euler angles from current pose
   rpyAngles = euler_from_quaternion(ref_transform.rotation)
   # and I just need the yaw
@@ -41,11 +50,17 @@ def refUpdate():
   # update the rotation as well
   yawAngle = yawAngle + delta_t*ref_yaw_scale*ref_velocity.angular.z
   ref_transform.rotation = quaternion_from_euler(0.0, 0.0, yawAngle)
+
   # publish the reference as a transform
-  pub_ref_tf.sendTransform((ref_transform.translation.x, ref_transform.translation.y, ref_transform.translation.z),
-                           ref_transform.rotation,
-                           rospy.Time.now(),
-                           drone_frame, global_frame)
+  # pub_ref_tf.sendTransform((ref_transform.translation.x, ref_transform.translation.y, ref_transform.translation.z),
+  #                         ref_transform.rotation,
+  #                         rospy.Time.now(),
+  #                         drone_frame, global_frame)
+
+  # and as a set of joint states for the URDF drone
+  js_msg.header.stamp = rospy.Time.now()
+  js_msg.position = [ref_transform.translation.x, ref_transform.translation.y, ref_transform.translation.z, yawAngle]
+  pub_joint_states.publish(js_msg)
 
 def refVelCallback(data):
   global ref_velocity
@@ -64,6 +79,7 @@ rospy.init_node('virtual_drone', anonymous=True)
 sub_ref_vel = rospy.Subscriber('ref_vel', Twist, refVelCallback)
 sub_ref_pos = rospy.Subscriber('ref_tf', TransformStamped, reftfCallback)
 pub_ref_tf = tf.TransformBroadcaster()
+pub_joint_states = rospy.Publisher('/joint_states', JointState)
 
 # and for reference position
 ref_transform = Transform()
